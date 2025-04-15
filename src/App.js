bb320435f224fb4d998142ac406ccc07511e229b
Gaurav Pratap Singh <gs492979@gmail.com>
2025-04-15 14:41:32 +0530

    all basic static component created.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-function App() {
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+// ];
+
+export default function App() {
   return (
-    <>
-      <h1>Travel List</h1>
-    </>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
   );
 }
 
-export default App;
+function Logo() {
+  return (
+    <>
+      <h1>🌴 Far Away 💼</h1>
+    </>
+  );
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip ?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">List</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on your list, and you already X (X%)</em>
+    </footer>
+  );
+}
