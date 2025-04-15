1cf5173b1eb2c5659219f9f841a59f6af8140bdd
Gaurav Pratap Singh <gs492979@gmail.com>
2025-04-15 15:08:25 +0530

    created static packing list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-// const initialItems = [
-//   { id: 1, description: "Passports", quantity: 2, packed: false },
-//   { id: 2, description: "Socks", quantity: 12, packed: false },
-// ];
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+  { id: 4, description: "Water Bottle", quantity: 12, packed: false },
+];
 
 export default function App() {
   return (
@@ -29,13 +31,35 @@ function Form() {
   );
 }
 
-function PackingList() {
-  return <div className="list">List</div>;
+function PackingList({ items }) {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Items item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 function Stats() {
   return (
     <footer className="stats">
       <em>💼 You have X items on your list, and you already X (X%)</em>
     </footer>
+  );
+}
+function Items({ item }) {
+  return (
+    <li>
+      <span
+        style={
+          item.packed ? { color: "lime", textDecoration: "line-through" } : {}
+        }
+      >
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
   );
 }
