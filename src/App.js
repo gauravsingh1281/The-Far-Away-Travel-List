cb5a42dba2fd74109531a2c9ab9b7015f9a1daee
Gaurav Pratap Singh <gs492979@gmail.com>
2025-04-18 15:29:55 +0530

    stats functionality added.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 export default function App() {
   const [items, setItems] = useState([]);
+
   function handleAddItems(item) {
     setItems((prevValue) => [...prevValue, item]);
   }
@@ -24,7 +25,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -88,10 +89,24 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
     </div>
   );
 }
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list. 🚀</em>
+      </p>
+    );
+  const itemsNum = items.length;
+  const packedItemNum = items.filter((item) => item.packed).length;
+  const percentage = Math.round((packedItemNum / itemsNum) * 100);
   return (
     <footer className="stats">
-      <em>💼 You have X items on your list, and you already X (X%)</em>
+      <em>
+        {percentage === 100
+          ? `You got everything! Ready to go ✈️`
+          : `💼 You have ${itemsNum} items on your list, and you already packed 
+        ${packedItemNum} (${!isNaN(percentage) ? percentage : 0}%)`}
+      </em>
     </footer>
   );
 }
