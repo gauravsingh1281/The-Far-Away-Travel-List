e8abbad99cdc641967eea80306f48cec9bd53dd1
Gaurav Pratap Singh <gs492979@gmail.com>
2025-04-18 13:59:15 +0530

    unused code removed.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: true },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 1, packed: false },
-  { id: 4, description: "Water Bottle", quantity: 12, packed: false },
-];
-
 export default function App() {
   const [items, setItems] = useState([]);
   function handleAddItems(item) {
